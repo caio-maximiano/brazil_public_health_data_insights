b4421a00763263da32c3c7c80baaef11b1af2b46
caio-maximiano <caio-maximiano>
2024-04-16 02:17:51 +0000

    first commit

---
+++ pipeline/extract_ftp_server.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Extraction
+# MAGIC  - Brazilian Healt Data resides in a FTP server maintained by the the government
+
+# COMMAND ----------
+
+
